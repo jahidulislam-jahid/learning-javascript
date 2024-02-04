d46012350eebf63eb0d2d6e2101a1ca68835496d
Jahid <md.jahidulislam.5423@gmail.com>
2024-02-04 19:04:17 +0600

    push pop and shift unshift fully updated

--- js array/push-pop.js
+++ js array/push-pop.js
@@ -11,3 +11,18 @@ numbers.push(100, 101, 102);
 
 console.log(numbers);
 
+const friends = ['balam', 'kalam', 'salam'];
+friends.push('kahilam');
+console.log(friends);
+
+// for removing last element of the array (pop)
+friends.pop();
+console.log(friends);
+
+const out1 = friends.pop();
+console.log(friends);
+
+const out2 = friends.pop();
+console.log(friends);
+
+console.log(out1, out2); // for placing the removed values into a new variable.
---
+++ js array/shift unshift.js
@@ -0,0 +1,10 @@
+const friends = ['balam', 'kalam', 'salam'];
+
+console.log(friends);
+
+friends.shift(); // for removing first element of the array
+console.log(friends);
+
+friends.unshift('joss'); // for adding element in the first in the array
+console.log(friends);
+
