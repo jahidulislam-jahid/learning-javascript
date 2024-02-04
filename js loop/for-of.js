336ecfa01575b1e07cbd2d76195a70089cf70a2d
Jahid <md.jahidulislam.5423@gmail.com>
2024-02-04 19:45:05 +0600

    practicing loops of js

---
+++ js loop/for-of.js
@@ -0,0 +1,16 @@
+const numbers = [12, 98, 45, 63, 21, 72, 52, 81, 19];
+
+console.log(numbers);
+
+// for showing the values of the array
+for(const num of numbers){ // for taking the values of numbers for num 
+    console.log(num);
+}
+
+// new practice
+const fruits = ['orange', 'apple', 'banana', 'jackfruit', 'watermelon'];
+
+for( const fruit of fruits){
+    console.log(fruit)
+    console.log('I want to eat');
+}
