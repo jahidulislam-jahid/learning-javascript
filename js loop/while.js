be6ad4f25adb38deb94ea8b4819b3e48de7b4751
Jahid <md.jahidulislam.5423@gmail.com>
2024-02-05 19:29:52 +0600

    while loop update

---
+++ js loop/while.js
@@ -0,0 +1,14 @@
+const number = 7;
+if(number < 10){
+    console.log('choto ekta number')
+}
+console.log('something new is cooking');
+
+// -------- While Loop-------------
+let num = 0; // loop variable
+while(n<5){
+    console.log('Looping' , num);
+    //num = num + 1;
+    // num += 1;
+    num++;
+}
