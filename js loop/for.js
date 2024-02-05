ed72392ea61bcdfd0115f4058e65cff5f0c4d482
Jahid <md.jahidulislam.5423@gmail.com>
2024-02-05 19:54:22 +0600

    updating

---
+++ js loop/for.js
@@ -0,0 +1,13 @@
+// let num = 0; // loop variable
+// while(num<5){ // loop condition
+//     console.log(num); // loop statement
+//     num++; // loop variable change
+// }
+
+// for(first part ; second part ; third part){
+//     loop statement;
+// }
+
+for(let num = 0; num < 5; num++){
+    console.log(num);
+}
