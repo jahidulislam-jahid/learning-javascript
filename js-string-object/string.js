eac22756ac5e195ef2b309ae6970c5969d908c54
Jahid <md.jahidulislam.5423@gmail.com>
2024-02-06 18:30:59 +0600

    string variable folder updating

---
+++ js-string-object/string.js
@@ -0,0 +1,23 @@
+const country = 'Bangladesh';
+const division = "Noakhali";
+const district = `Bramhanbaria`;
+//const thana = new String('Demra');
+
+// console.log(typeof country);
+// console.log(typeof division);
+// console.log(typeof district);
+
+
+const numbers = [54, 98, 78, 21, 65];
+// console.log(numbers.length);
+// console.log(numbers[0]);
+// console.log(numbers[1]);
+
+numbers[1] = 11;
+console.log(numbers);
+
+const capital = "Dhaka";
+console.log(capital);
+console.log(capital.length);
+console.log(capital[0]);
+console.log(capital[1]);
