0d65c5f74009005ddae04f000879a4544cdce9e6
Jahid <md.jahidulislam.5423@gmail.com>
2024-02-06 19:34:31 +0600

    objects updating

---
+++ js-string-object/object.js
@@ -0,0 +1,35 @@
+// primitive 
+// basic, primary datatype
+
+const age = 21;
+const school = "rifle square";
+const isPassed = true;
+//let isDeveloper;
+const subjects = ['Bangla', 'English', 'Physics', 'Math'];
+
+//console.log(isDeveloper);
+
+const bottle_wrong = ["white", 45];
+const bottleColor = 'white';
+const bottlePrice = 45;
+const bottleDrinks = 'water';
+
+//object --> non primitive 
+const bottle = {
+    brand: 'apple',
+    price: 45,
+    color: 'white',
+    isClean: false
+
+}
+
+const subject = {
+    name: 'biology',
+    teacher: 'rasheda madam',
+    examDate: '30 Feb',
+    chapters: ['first','second','third'],
+    exams: {
+        name: 'final exam',
+        marks: 100
+    }
+}
