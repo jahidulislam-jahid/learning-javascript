95d27c67c310d788e0a8f8e1d7fa0cf16045615f
Jahid <md.jahidulislam.5423@gmail.com>
2024-02-06 18:05:11 +0600

    updating more

---
+++ js loop/break.js
@@ -0,0 +1,20 @@
+for (let i = 0; i < 15; i++) {
+    console.log(i);
+    if (i >= 10) {
+        break;
+    }
+}
+
+console.log('Life After Breakup');
+
+
+let n = 54;
+while (n > 25) {
+
+    if (n <= 50) {
+        break;
+    }
+
+    console.log(n);
+    n--;
+}
